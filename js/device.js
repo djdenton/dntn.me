ac707fb54408eb65afc62ed55043afb19585b133
Daniel Denton <daniel.james.denton@gmail.com>
2018-04-17 21:36:18 +0100

    Initial Commit

---
+++ js/device.js
@@ -0,0 +1,25 @@
+  // Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBFErnCaSdbeJMKjkpz_-Fn3emriLwIOLI",
+    authDomain: "dictation-prototype.firebaseapp.com",
+    databaseURL: "https://dictation-prototype.firebaseio.com",
+    projectId: "dictation-prototype",
+    storageBucket: "dictation-prototype.appspot.com",
+    messagingSenderId: "805593500412"
+  };
+firebase.initializeApp(config);
+
+// Get document
+var dictation = firebase.database().ref('/Dictation');
+
+// Listen for changes to document
+dictation.on('value', function(snapshot) {
+  document.getElementById('receiver').innerHTML = snapshot.val();                   
+});
+
+// Update Document
+var device = document.getElementById('device');
+
+device.addEventListener('input', function() {
+    dictation.set(device.innerHTML);
+});
