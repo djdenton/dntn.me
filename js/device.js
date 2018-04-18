d14f3f37b0cca4cc416dc35cb6bac6283cfc5ee5
Daniel Denton <daniel.james.denton@gmail.com>
2018-04-18 22:59:47 +0100

    Active Section Update
    Rewrote code so that only the current section is kept

--- js/device.js
+++ js/device.js
@@ -9,19 +9,41 @@ var config = {
   };
 firebase.initializeApp(config);
 
-// Get document
+// Get document. Set NewParagraph to false and clear the dictation
 var dictation = firebase.database().ref('/Dictation');
-
-// Listen for changes to document
-dictation.on('value', function(snapshot) {
-  document.getElementById('receiver').innerHTML = snapshot.val();                   
-});
+dictation.child('ActiveSection').set('');
+dictation.child('NewParagraph').set(false);
 
 window.onload=function(){
-// Update Document
-var device = document.getElementById('device');
 
-device.addEventListener('input', function() {
-    dictation.set(device.innerHTML);
-});
+  // Define sections
+  var device = document.getElementById('Device');
+  var activeSection = document.getElementById('ActiveSection');
+  var receiver = document.getElementById('Receiver');
+  var main = document.getElementById('Main');
+  var update = document.getElementById('Update');
+
+  // Listen for changes to the database and update the active section and receiver
+  dictation.on('value', function(snapshot) {
+    // If there is a new paragraph
+    if(snapshot.val().NewParagraph){
+      main.innerHTML = main.innerHTML + snapshot.val().ActiveSection; //Only needed on web version
+      dictation.child('NewParagraph').set(false); //Reset the new para
+    };
+    activeSection.innerHTML = snapshot.val().ActiveSection;
+    update.innerHTML = snapshot.val().ActiveSection;
+  });
+
+  // Update Document
+  device.addEventListener('input', function() {
+    // If there is a new paragraph
+    if(activeSection.innerHTML.includes('<div><br></div>')){
+      dictation.child('ActiveSection').set(device.innerHTML);
+      dictation.child('NewParagraph').set(true);
+      device.innerHTML = '';
+      activeSection.innerHTML = '';
+    }else{
+      dictation.child('ActiveSection').set(device.innerHTML);
+    };
+  });
 }
